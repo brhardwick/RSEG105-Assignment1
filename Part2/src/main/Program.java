072a325687ff659a69b12e4b7b438718a119f680
Brad Hardwick <brad.hardwick@pes-companies.com>
2017-11-01 07:15:24 -0400

    Stripped out spring boot part 2

---
+++ Part2/src/main/Program.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.print("We started.");
+		return;
+
+	}
+
+}
